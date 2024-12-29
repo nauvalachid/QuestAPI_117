2207fd0b589699b01f10c12d7265653635cbcf96
nauvalachid <nauvalachid@gmail.com>
2024-12-30 01:14:35 +0700

    implementasi layar EntryMhsScreen dengan logika penyimpanan data Mahasiswa

--- app/src/main/java/com/example/pokedex/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pokedex/ui/view/InsertView.kt
@@ -1,8 +1,73 @@
 package com.example.pokedex.ui.view
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pokedex.Navigation.DestinasiNavigasi
+import com.example.pokedex.ui.customwidget.CostumeTopAppBar
+import com.example.pokedex.ui.viewmodel.InsertUiEvent
+import com.example.pokedex.ui.viewmodel.InsertUiState
+import com.example.pokedex.ui.viewmodel.InsertViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi{
     override val route = "item_entry"
     override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack:()-> Unit,
+    modifier: Modifier =Modifier,
+    viewModel:InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent)->Unit,
+    onSaveClick:()->Unit,
+    modifier: Modifier=Modifier
+){
+
 }
