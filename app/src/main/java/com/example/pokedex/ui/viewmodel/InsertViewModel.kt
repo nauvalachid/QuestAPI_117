7bb8314b88337aa2ee13139e3d36c8ef684d8df6
nauvalachid <nauvalachid@gmail.com>
2024-12-29 16:21:36 +0700

    Menambahkan InsertViewModel untuk mengelola status UI dan event input mahasiswa

--- app/src/main/java/com/example/pokedex/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/viewmodel/InsertViewModel.kt
@@ -1,2 +1,16 @@
 package com.example.pokedex.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pokedex.repository.MahasiswaRepository
+
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent.InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+}
