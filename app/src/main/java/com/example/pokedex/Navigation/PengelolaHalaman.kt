94095c312397a2f9ecf66636130b7a3c632e8664
nauvalachid <nauvalachid@gmail.com>
2024-12-30 03:22:23 +0700

    menambahkan komposisi MahasiswaApp sebagai kerangka utama aplikasi

--- app/src/main/java/com/example/pokedex/Navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pokedex/Navigation/PengelolaHalaman.kt
@@ -1,2 +1,39 @@
 package com.example.pokedex.Navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.pokedex.ui.view.DestinasiEntry
+import com.example.pokedex.ui.view.DestinasiHome
+import com.example.pokedex.ui.view.EntryMhsScreen
+import com.example.pokedex.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController:NavHostController= rememberNavController()){
+    NavHost(
+        navController=navController,
+        startDestinastion=DestinasiHome.route,
+        modifier=Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry={navController.navigate(DestinasiEntry)},
+                onDetailClick={
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    PopUpTo(DestinasiHome.route){
+                        inclusive=true
+                    }
+                }
+            })
+        }
+    }
+}
