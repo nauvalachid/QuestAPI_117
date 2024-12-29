f55cde367299a4217b31c57990e9cbb367df2ac1
nauvalachid <nauvalachid@gmail.com>
2024-12-29 16:33:13 +0700

    menambahkan fungsi untuk mengonversi objek Mahasiswa menjadi InsertUiEvent

--- app/src/main/java/com/example/pokedex/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/viewmodel/InsertViewModel.kt
@@ -53,3 +53,12 @@ fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
