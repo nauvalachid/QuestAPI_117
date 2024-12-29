f70c0db2bdd23c06cc1006041704f3dbb50847b0
nauvalachid <nauvalachid@gmail.com>
2024-12-29 16:11:20 +0700

    Menambahkan class MahasiswaApplications untuk inisialisasi AppContainer

--- app/src/main/java/com/example/pokedex/MahasiswaApplications.kt
+++ app/src/main/java/com/example/pokedex/MahasiswaApplications.kt
@@ -1,2 +1,13 @@
 package com.example.pokedex
 
+import android.app.Application
+import com.example.pokedex.repository.AppContainer
+import com.example.pokedex.repository.MahasiwaContainer
+
+class MahasiswaApplications:Application(){
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container=MahasiwaContainer()
+    }
+}
