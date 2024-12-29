813076d8d138cfd84d3b4aa94e561490e8621148
nauvalachid <nauvalachid@gmail.com>
2024-12-30 00:48:49 +0700

    implementasi HomeViewModel dengan pengambilan data Mahasiswa

--- app/src/main/java/com/example/pokedex/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/viewmodel/HomeViewModel.kt
@@ -1,9 +1,40 @@
 package com.example.pokedex.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pokedex.model.Mahasiswa
+import com.example.pokedex.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
     object Error:HomeUiState()
     object Loading:HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e:HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
 }
