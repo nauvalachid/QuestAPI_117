0f8f116e24b9ed2baed0a34bd8d05a2944879038
nauvalachid <nauvalachid@gmail.com>
2024-12-30 02:07:36 +0700

    menambahkan objek DestinasiHome untuk rute dan judul Home Mahasiswa

--- app/src/main/java/com/example/pokedex/ui/view/HomeView.kt
+++ app/src/main/java/com/example/pokedex/ui/view/HomeView.kt
@@ -1,2 +1,8 @@
 package com.example.pokedex.ui.view
 
+import com.example.pokedex.Navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
