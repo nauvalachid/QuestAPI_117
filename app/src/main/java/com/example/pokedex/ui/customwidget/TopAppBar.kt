8e35f40fee8ee77feb409d8882d2725c1e827712
nauvalachid <nauvalachid@gmail.com>
2024-12-30 01:00:08 +0700

    implementasi CenterAlignedTopAppBar dengan tombol refresh

--- app/src/main/java/com/example/pokedex/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/pokedex/ui/customwidget/TopAppBar.kt
@@ -1,6 +1,12 @@
 package com.example.pokedex.ui.customwidget
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,5 +21,12 @@ fun CostumeTopAppBar(
     navigateUp: ()-> Unit = {},
     onRefresh: ()-> Unit ={},
 ){
-
+    CenterAlignedTopAppBar(
+        title = {Text(title)},
+        actions = {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "",modifier=Modifier.clickable {
+                onRefresh()
+            })
+        }
+    )
 }
