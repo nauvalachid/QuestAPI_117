fc7226a8333ce065988eb38a4b2cea0bb44b3e0d
nauvalachid <nauvalachid@gmail.com>
2024-12-30 00:43:36 +0700

    tambah HomeUiState untuk manajemen status UI

--- app/src/main/java/com/example/pokedex/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/viewmodel/HomeViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.pokedex.ui.viewmodel
 
+import com.example.pokedex.model.Mahasiswa
+
+sealed class HomeUiState {
+    data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
+    object Error:HomeUiState()
+    object Loading:HomeUiState()
+}
