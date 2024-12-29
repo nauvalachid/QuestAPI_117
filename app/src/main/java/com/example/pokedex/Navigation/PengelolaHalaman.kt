204674f872361e07d5535984dbdc60e5be3642c2
nauvalachid <nauvalachid@gmail.com>
2024-12-30 03:29:42 +0700

    perbaikan PengelolaHalaman.kt

--- app/src/main/java/com/example/pokedex/Navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pokedex/Navigation/PengelolaHalaman.kt
@@ -6,6 +6,8 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
 import androidx.navigation.compose.rememberNavController
 import com.example.pokedex.ui.view.DestinasiEntry
 import com.example.pokedex.ui.view.DestinasiHome
@@ -13,27 +15,28 @@ import com.example.pokedex.ui.view.EntryMhsScreen
 import com.example.pokedex.ui.view.HomeScreen
 
 @Composable
-fun PengelolaHalaman(navController:NavHostController= rememberNavController()){
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
     NavHost(
-        navController=navController,
-        startDestinastion=DestinasiHome.route,
-        modifier=Modifier,
-    ){
-        composable(DestinasiHome.route){
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry={navController.navigate(DestinasiEntry)},
-                onDetailClick={
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
                 }
             )
         }
-        composable(DestinasiEntry.route){
-            EntryMhsScreen(navigateBack = {
-                navController.navigate(DestinasiHome.route){
-                    PopUpTo(DestinasiHome.route){
-                        inclusive=true
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
                     }
                 }
-            })
+            )
         }
     }
 }
+
