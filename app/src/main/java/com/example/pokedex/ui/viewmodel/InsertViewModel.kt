3f3dac7208a959805da775d5a7f502b5f0c62b80
nauvalachid <nauvalachid@gmail.com>
2024-12-29 16:23:39 +0700

    Menambahkan fungsi suspend insertMhs untuk menyisipkan data mahasiswa dengan penanganan error

--- app/src/main/java/com/example/pokedex/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/viewmodel/InsertViewModel.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pokedex.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
     var uiState by mutableStateOf(InsertUiState())
@@ -12,5 +14,15 @@ class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
 
     fun updateInsertMhsState(insertUiEvent.InsertUiEvent) {
         uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
     }
 }
