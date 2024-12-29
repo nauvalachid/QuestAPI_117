1a46220e74260eff2f42565f3a5762c01689e569
nauvalachid <nauvalachid@gmail.com>
2024-12-30 00:57:30 +0700

    menambahkan  komponen CostumeTopAppBar dengan dukungan navigasi dan refresh

--- app/src/main/java/com/example/pokedex/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/pokedex/ui/customwidget/TopAppBar.kt
@@ -1,2 +1,19 @@
 package com.example.pokedex.ui.customwidget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: ()-> Unit = {},
+    onRefresh: ()-> Unit ={},
+){
+
+}
