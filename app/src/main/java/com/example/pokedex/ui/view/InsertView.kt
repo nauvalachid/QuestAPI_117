ea89706ac859c4b8d38c8ba1fc482d205e82ffab
nauvalachid <nauvalachid@gmail.com>
2024-12-30 01:05:18 +0700

    menambahkan objek DestinasiEntry untuk rute dan judul Entry Mahasiswa

--- app/src/main/java/com/example/pokedex/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pokedex/ui/view/InsertView.kt
@@ -1,2 +1,8 @@
 package com.example.pokedex.ui.view
 
+import com.example.pokedex.Navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
