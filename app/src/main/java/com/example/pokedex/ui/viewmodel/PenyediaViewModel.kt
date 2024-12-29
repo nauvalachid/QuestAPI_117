fa873d89cc9830b10d0c7c4bb0108d0edf465784
nauvalachid <nauvalachid@gmail.com>
2024-12-30 01:59:28 +0700

    Menambahkan objek PenyediaViewModel untuk menyediakan instansi ViewModel

--- app/src/main/java/com/example/pokedex/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.pokedex.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pokedex.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+        initializer {
+            InsertViewModel(
+                aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+    }
+}
+
+    fun CreationExtras.aplikasiMahasiswa():MahasiswaApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
